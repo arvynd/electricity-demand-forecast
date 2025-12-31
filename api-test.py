7fe9deeef5567b1bcebcc6cd9170212498a8b01d
Aravind Pramod <aravind.pramod@o9solutions.net>
2025-12-31 23:32:59 +0530

    Updated API Ingestion with filters and pagination

--- api-test.py
+++ api-test.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 load_dotenv()
 
+
 EIA_API_KEY = os.getenv("EIA_API_KEY")
 if not EIA_API_KEY:
     raise RuntimeError("EIA_API_KEY not set")
@@ -14,11 +15,35 @@ base_url = "https://api.eia.gov/v2/electricity/rto/region-data/data/"
 params = {
     "api_key": EIA_API_KEY,
     "data[0]": "value",
+    "facets[type][]": "D",
+    "length": 5000,
+    "start": "2023-01-01",
+    "end": "2023-01-02",
 }
 
-response = requests.get(base_url, params=params, timeout=10)
-response.raise_for_status()
+all_rows = []
+offset = 0
+page_size = 5000
 
-data = response.json()
+while True:
+    params["offset"] = offset
 
-df = pd.DataFrame(data["response"]["data"])
+    response = requests.get(
+        "https://api.eia.gov/v2/electricity/rto/region-data/data/",
+        params=params,
+        timeout=60,
+    )
+    response.raise_for_status()
+
+    batch = response.json()["response"]["data"]
+    if not batch:
+        break
+
+    all_rows.extend(batch)
+
+    if len(batch) < page_size:
+        break
+
+    offset += page_size
+
+df = pd.DataFrame(all_rows)
